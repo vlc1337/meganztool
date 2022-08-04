92b6a95e841de6bf7b038865a1d2ec6fec314592
vlc1337 <95694878+vlc1337@users.noreply.github.com>
2022-08-04 14:45:44 +0300

    Update megatool.py

--- megatool.py
+++ megatool.py
@@ -1,11 +1,13 @@
-import colorama, os, time, ctypes
+import colorama, os, time, ctypes, datetime, random, string
 from os import system
+from datetime import datetime
 from mega import Mega
 from colorama import Fore, Style, init
 import traceback
+from datetime import datetime
 colorama.init(autoreset=True)
 
-ctypes.windll.kernel32.SetConsoleTitleW("MEGA.NZ TOOL | MADE BY VLC")
+ctypes.windll.kernel32.SetConsoleTitleW("MEGA.NZ TOOL | MADE BY VLC | VERSION 0.3")
 
 cls = lambda: os.system('cls') if os.name == 'nt' else os.system('clear')
 mega = Mega()
@@ -20,10 +22,10 @@ logo = f'''
  {Fore.RED} ▐    ▐     ▐                           ▐                      ▐                             ▐         
 
                             {Fore.WHITE} >>> https://github.com/vlc1337 <<<
-                            
-info - get information about account     
+                                
 checker - check several accounts from txt              
 upload - upload a file
+massupload - upload a file to several accs
 creator - accounts creator
 
 If you want to leave when you got an error just restart the programm'''
@@ -36,59 +38,29 @@ def upload():
             mail = acc[0]
             passw = acc[1]
         except:
-            print(Fore.RED + '[ERROR] Invalid input')
+            print(Fore.RED + '\n[ERROR] Invalid input')
             upload() 
         try:
             m = mega.login(mail, passw)
         except:
-            print(Fore.RED + '[ERROR] Account may be invalid or your internet connection is down')
+            print(Fore.RED + '\n[ERROR] Account may be invalid or your internet connection is down')
             upload()
         path = input(Fore.RED + '\nInput path to your file: ' + Fore.BLUE)
         try:
             os.chdir(path)
         except:
-            print(Fore.RED + '[ERROR] This path does not exist')
+            print(Fore.RED + '\n[ERROR] This path does not exist')
             upload()
         filename = input(Fore.RED + '\nInput filename(example: bebra.txt): ' + Fore.BLUE)
         try:
             file = m.upload(filename)
-            print('Success! Link: ' + m.get_upload_link(file))
+            print('\nSuccess! Link: ' + m.get_upload_link(file))
         except:
-            print(Fore.RED + '[ERROR] This file does not exist')
+            print(Fore.RED + '\n[ERROR] This file does not exist')
             upload()
         input(Fore.RED + '\nPress enter to continue.')
     except KeyboardInterrupt:
         print('\nStopped programm. Exiting...')
-        quit()
-
-def info():
-    try:
-        mailpass = input(Fore.RED + '\nInput login information(mail:pass) ' + Fore.BLUE)
-        acc = mailpass.split(':')
-        mail = acc[0]
-        passw = acc[1]
-        try:
-            m = mega.login(mail, passw)
-            quota = m.get_quota()
-            print(f'{Fore.RED}{quota} KB is free')
-            balance = m.get_balance()
-            print(f'{Fore.RED}Balance is {balance}$')
-        except:
-            print(Fore.RED + '[ERROR] Account may be invalid or your internet connection is down')
-            info()
-        input(Fore.RED + '\nPress enter to continue.')
-    except KeyboardInterrupt:
-        print('\n{Fore.RED}Stopped programm. Exiting...')
-        quit()
-
-def ww():
-    try:
-        amount = int(input(Fore.RED + 'Amount of links you want to generate and check: ' + Fore.BLUE))
-        value = 1
-        while value <= amount:
-            value += 1
-    except KeyboardInterrupt:
-        print('\n{Fore.RED}Stopped programm. Exiting...')
         quit()
 
 def checker():
@@ -99,17 +71,18 @@ def checker():
         try:
             os.chdir(path)
         except:
-            print(Fore.RED + '[ERROR] This path does not exist')
+            print(Fore.RED + '\n[ERROR] This path does not exist')
             checker()
-        file = input(Fore.RED + '\nInput filename(example: bebra): ' + Fore.BLUE)
+        file = input(Fore.RED + '\nInput filename(example: bebra.txt): ' + Fore.BLUE)
         try:
-            f = open(f'{file}.txt', 'r')
+            f = open(f'{file}', 'r')
             f.close()
         except:
-            print('[ERROR] This file does not exist')
+            print('\n[ERROR] This file does not exist')
             checker()
-        input(Fore.RED + 'Press enter to start checker')
-        with open(f'{file}.txt', 'r', encoding='utf-8') as accounts:
+        input(Fore.RED + '\nPress enter to start checker')
+        start_time = datetime.now()
+        with open(f'{file}', 'r', encoding='utf-8') as accounts:
             accounts = accounts.read()
             if len(accounts) >= 1:
                 account = accounts.split('\n')
@@ -129,14 +102,77 @@ def checker():
 Check finished.
 Valid accounts: {Fore.LIGHTGREEN_EX}{valid}{Fore.RESET}
 Invalid accounts: {Fore.RED}{invalid}{Fore.RESET}
-Total accounts: {valid+invalid}''')
+Total accounts: {Fore.MAGENTA}{valid+invalid}{Fore.RESET}
+Time passed: {Fore.MAGENTA}{datetime.now() - start_time}{Fore.RESET}
+Valid accounts saved in the same path as the accounts''')
+        input(Fore.RED + '\nPress enter to continue.')
+    except KeyboardInterrupt:
+        print('\n{Fore.RED}Stopped programm. Exiting...')
+        quit()
+
+def massupload():
+    success = 0
+    err = 0
+    try:
+        path = input(Fore.RED + '\nInput path to txt with accounts: ' + Fore.BLUE)
+        try:
+            os.chdir(path)
+        except:
+            print(Fore.RED + '\n[ERROR] This path does not exist')
+            massupload()
+        file = input(Fore.RED + '\nInput filename with accounts(example: bebra.txt): ' + Fore.BLUE)
+        try:
+            f = open(f'{file}', 'r')
+            f.close()
+        except:
+            print('\n[ERROR] This file does not exist')
+            massupload()
+        path = input(Fore.RED + '\nInput path to the file you want to upload: ' + Fore.BLUE)
+        try:
+            os.chdir(path)
+        except:
+            print(Fore.RED + '\n[ERROR] This path does not exist')
+            massupload()
+        filename = input(Fore.RED + '\nInput filename(example: bebra.txt): ' + Fore.BLUE)
+        try:
+            f = open(f'{file}', 'r')
+            f.close()
+        except:
+            print('\n[ERROR] This file does not exist')
+            massupload()
+        input(Fore.RED + '\nPress enter to start mass upload')
+        start_time = datetime.now()
+        with open(f'{file}', 'r', encoding='utf-8') as accounts:
+            accounts = accounts.read()
+            if len(accounts) >= 1:
+                account = accounts.split('\n')
+        for account in account:
+            try:
+                acc = account.split(':')
+                z = mega.login(acc[0], acc[1])
+                file = z.upload(filename)
+                z.get_upload_link(file)
+                print(f'{Fore.WHITE}Account: {Fore.GREEN}{account} {Fore.WHITE}Link to uploaded file: {Fore.GREEN}{z.get_upload_link(file)}')
+                output = open('uploadedlinks.txt', 'a', encoding='utf-8')
+                output.write(z.get_upload_link(file) + '\n')
+                output.close()
+                success += 1
+            except:
+                print(f"{Fore.RED}[ERROR] Account: {account}")
+                err += 1
+        print(f'''
+Mass upload finished.
+Success: {Fore.LIGHTGREEN_EX}{success}{Fore.RESET}
+Errors: {Fore.RED}{err}{Fore.RESET}
+Time passed: {Fore.MAGENTA}{datetime.now() - start_time}{Fore.RESET}
+Links to the files saved in the same path as the accounts''')
         input(Fore.RED + '\nPress enter to continue.')
     except KeyboardInterrupt:
         print('\n{Fore.RED}Stopped programm. Exiting...')
         quit()
 
 def creator():
-    print(Fore.WHITE + 'https://github.com/vlc1337/meganzaccountcreator (also read readme for this soft)')
+    print(Fore.WHITE + '\nhttps://github.com/vlc1337/meganzaccountcreator (also read readme for this soft)')
     input(Fore.RED + '\nPress enter to continue.')
 
 while True:
@@ -146,12 +182,12 @@ while True:
         select = str(input(Fore.RED + '\nInput command: ' + Fore.BLUE))
         if select == 'upload':
             upload()
-        elif select == 'info':
-            info()
         elif select == 'checker':
             checker()
         elif select == 'creator':
             creator()
+        elif select == 'massupload':
+            massupload()
         else:
             print(Fore.RED + '\nCommand does not exist. ')
             nextCode = input('\nPress enter to continue.')
